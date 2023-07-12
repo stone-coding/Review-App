16b5d8b1c07d56c26002ba93b449d7edb0adf368
stone-coding <hxs649@case.edu>
2023-07-12 09:31:11 -0400

    finished Dashboard UI for movie and actor, next is rendering movie and actor from backend

--- frontend/src/components/admin/Actors.jsx
+++ frontend/src/components/admin/Actors.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BsPencilSquare, BsTrash } from "react-icons/bs";
 
 export default function Actors() {
   return (
-    <div>
-      Actors
+    <div className="grid grid-cols-4 gap-3 my-5">
+      <ActorProfile
+        profile={{
+          name: "John Doe",
+          avatar:
+            "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          about:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ab assumenda saepe? Voluptatibus repudiandae dolore molestias quos omnis accusantium autem at vitae praesentium voluptatem! Enim sint totam soluta mollitia ratione.",
+        }}
+      />
+      <ActorProfile
+        profile={{
+          name: "John Doe",
+          avatar:
+            "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          about:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ab assumenda saepe? Voluptatibus repudiandae dolore molestias quos omnis accusantium autem at vitae praesentium voluptatem! Enim sint totam soluta mollitia ratione.",
+        }}
+      />
+      <ActorProfile
+        profile={{
+          name: "John Doe",
+          avatar:
+            "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          about:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ab assumenda saepe? Voluptatibus repudiandae dolore molestias quos omnis accusantium autem at vitae praesentium voluptatem! Enim sint totam soluta mollitia ratione.",
+        }}
+      />
+      <ActorProfile
+        profile={{
+          name: "John Doe",
+          avatar:
+            "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          about:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ab assumenda saepe? Voluptatibus repudiandae dolore molestias quos omnis accusantium autem at vitae praesentium voluptatem! Enim sint totam soluta mollitia ratione.",
+        }}
+      />
     </div>
-  )
+  );
 }
+
+const ActorProfile = ({ profile }) => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowOptions(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowOptions(false);
+  };
+
+  if (!profile) return null;
+
+  const { avatar, name, about = " " } = profile;
+
+  return (
+    <div className="bg-white shadow dark:shadow dark:bg-secondary rounded h-20 overflow-hidden">
+      <div
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="flex cursor-pointer relative"
+      >
+        <img
+          src={avatar}
+          alt={name}
+          className="w-20 aspect-square object-cover"
+        />
+
+        <div className="px-2">
+          <h1 className="text-xl text-primary dark:text-white font-semibold">
+            {name}
+          </h1>
+          <p className="text-primary dark:text-white ">
+            {about.substring(0, 50)}
+          </p>
+        </div>
+
+        <Options visible={showOptions} />
+      </div>
+    </div>
+  );
+};
+
+const Options = ({ visible, onDeleteClick, onEditClick }) => {
+  if (!visible) return null;
+  return (
+    <div className="absolute inset-0 bg-primary bg-opacity-25 backdrop-blur-sm flex justify-center items-center space-x-5">
+      <button
+        onClick={onDeleteClick}
+        className="p-2 rounded-full bg-red-600 text-primary hover:opacity-80 transition"
+        type="button"
+      >
+        <BsTrash />
+      </button>
+      <button
+        onClick={onEditClick}
+        className="p-2 rounded-full bg-green-600 text-primary hover:opacity-80 transition"
+        type="button"
+      >
+        <BsPencilSquare />
+      </button>
+    </div>
+  );
+};
--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,4 +1,14 @@
+import AppInfoBox from "../AppInfoBox";
+import LatestUpLoads from "../LatestUpLoads";
 
 export default function Dashboard() {
-  return null
+  return (
+    <div className="grid grid-cols-3 gap-5 my-5">
+      <AppInfoBox title="Total Uploads" subtitle="100" />
+      <AppInfoBox title="Total Reviews" subtitle="1,500" />
+      <AppInfoBox title="Total Users" subtitle="200" />
+
+      <LatestUpLoads/>
+    </div>
+  );
 }
