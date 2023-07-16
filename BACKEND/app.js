550d9862091be743c8002e74026ed6acdadd4fa7
stone-coding <hxs649@case.edu>
2023-07-16 11:13:35 -0400

    done Review API

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 const userRouter = require("./routes/user");
 const actorRouter = require("./routes/actor")
 const movieRouter = require("./routes/movie")
+const reviewRouter = require("./routes/review")
 const { errorHandler } = require("./middlewares/error");
 const cors = require("cors");
 const { handleNotFound } = require("./utils/helper");
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/actor", actorRouter);
 app.use("/api/movie", movieRouter);
+app.use("/api/review", reviewRouter);
 app.use("/*", handleNotFound);
 
 app.use(errorHandler);
--- BACKEND/middlewares/auth.js
+++ BACKEND/middlewares/auth.js
@@ -3,29 +3,27 @@ const { sendError } = require("../utils/helper");
 const User = require("../models/user");
 
 exports.isAuth = async (req, res, next) => {
-    const token = req.headers?.authorization;
-  
-    if(!token) return sendError(res, "Invalid token!");
-    const jwtToken = token.split("Bearer ")[1];
-  
-    if (!jwtToken) return sendError(res, "Invalid token!");
-    const decode = jwt.verify(jwtToken, process.env.JWT_SECRET);
+  const token = req.headers?.authorization;
+
+  if (!token) return sendError(res, "Invalid token!");
+  const jwtToken = token.split("Bearer ")[1];
+
+  if (!jwtToken) return sendError(res, "Invalid token!");
+  const decode = jwt.verify(jwtToken, process.env.JWT_SECRET);
   //   const jwtRes = jwt.verify(jwtToken, process.env.JWT_SECRET)
   //   res.json({jwtRes})
-    const { userId } = decode;
-  
-    const user = await User.findById(userId);
-    if (!user) return sendError(res, "Invalid token user not found!", 404);
+  const { userId } = decode;
 
-    req.user = user
-    next();
+  const user = await User.findById(userId);
+  if (!user) return sendError(res, "Unauthorized access, Bearer code expires!", 404);
 
-  };
+  req.user = user;
+  next();
+};
 
+exports.isAdmin = async (req, res, next) => {
+  const { user } = req;
+  if (user.role !== "admin") return sendError(res, "unauthorized access!");
 
-  exports.isAdmin = (req, res, next) => {
-    const {user} = req;
-    if(user.role !== 'admin') return sendError(res, 'unauthorized access!')
-    
-    next()
-  }
+  next();
+};
