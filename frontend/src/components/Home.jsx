e875ece37b5f07530dbada3ed4b6190283a54a0c
stone-coding <hxs649@case.edu>
2023-07-18 22:05:03 -0400

    slide show works but will flash after each transition, need a fix

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,17 +6,21 @@ import NotVerified from "./User/NotVerified";
 import TopRatedMovies from "./User/TopRatedMovies";
 import TopRatedWebSeries from "./User/TopRatedWebSeries";
 import TopRatedTVSeries from "./User/TopRatedTVSeries";
+import HeroSlideShow from "./User/HeroSlideShow";
 
 export default function Home() {
   return (
     <div className="dark:bg-primary bg-white min-h-screen">
-      <Container>
+      <Container className='px-2 xl:p-0'>
         <NotVerified />
         {/* slider */}
+        <HeroSlideShow/>
         {/* Most rated movies */}
+        <div className="space-y-3 py-8">
         <TopRatedMovies />
         <TopRatedWebSeries />
         <TopRatedTVSeries />
+        </div>
       </Container>
     </div>
   );
